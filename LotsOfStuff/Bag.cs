97fc78f2a04578da8cf0dc23a31edfa79707c3ba
ineesgoncalvees <ines98goncalves@gmail.com>
2018-05-10 11:36:02 +0100

    Usar List

--- LotsOfStuff/Bag.cs
+++ LotsOfStuff/Bag.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
 
 namespace Aula11
 {
@@ -9,7 +9,7 @@ namespace Aula11
     public class Bag : IStuff
     {
         /// <summary>Array que contém os itens da mochila</summary>
-        private ArrayList stuff;
+        private List<IStuff> stuff;
 
         /// <summary>Número de itens na mochila</summary>
         public int StuffCount { get { return stuff.Count; } }
@@ -62,7 +62,7 @@ namespace Aula11
         /// </param>
         public Bag(int bagSize)
         {
-            stuff = new ArrayList(bagSize);
+            stuff = new List<IStuff>(bagSize);
         }
 
         /// <summary>Colocar um item na mochila</summary>
@@ -85,7 +85,7 @@ namespace Aula11
                 throw new InvalidOperationException(
                     "Bag doesn't have that much stuff!");
             }
-            return stuff[index] as IStuff;
+            return stuff[index];
         }
 
         /// <summary>
